aae65a6b76e9a3ba659b038c1e74b3598ea45b40
tkint <t.kint@hotmail.fr>
2017-03-17 00:17:13 +0100

    Ajout d'un jeu de données

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+user_group_list = [
+    ['Admin', 100],
+    ['User', 1]
+]
+
+user_list = [
+    ['Thomas', 'Kint', 1, 't.kint@hotmail.fr', 'password', 'password']
+]
+
+user_group_list.each do |name, level|
+  UserGroup.create(name: name, level: level)
+end
+
+user_list.each do |first_name, last_name, user_group_id, email, password, password_confirmation|
+  User.create!(first_name: first_name, last_name: last_name, user_group_id: user_group_id, email: email, password: password, password_confirmation: password_confirmation)
+end
