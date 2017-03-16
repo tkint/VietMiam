258927905d58d6f5c1e2981ae630a5536d02eb6f
tkint <t.kint@hotmail.fr>
2017-03-17 00:40:55 +0100

    Ajout d'un set de data pour la prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,36 @@
-user_group_list = [
-    ['Admin', 100],
-    ['User', 1]
-]
+case Rails.env
+  when 'development'
+    user_group_list = [
+        ['Admin', 100],
+        ['User', 1]
+    ]
 
-user_list = [
-    ['Thomas', 'Kint', 1, 't.kint@hotmail.fr', 'password', 'password']
-]
+    user_list = [
+        ['Thomas', 'Kint', 1, 't.kint@hotmail.fr', 'password', 'password']
+    ]
 
-user_group_list.each do |name, level|
-  UserGroup.create(name: name, level: level)
-end
+    user_group_list.each do |name, level|
+      UserGroup.create(name: name, level: level)
+    end
 
-user_list.each do |first_name, last_name, user_group_id, email, password, password_confirmation|
-  User.create!(first_name: first_name, last_name: last_name, user_group_id: user_group_id, email: email, password: password, password_confirmation: password_confirmation)
+    user_list.each do |first_name, last_name, user_group_id, email, password, password_confirmation|
+      User.create!(first_name: first_name, last_name: last_name, user_group_id: user_group_id, email: email, password: password, password_confirmation: password_confirmation)
+    end
+  when 'production'
+    user_group_list = [
+        ['Admin', 100],
+        ['User', 1]
+    ]
+
+    user_list = [
+        ['Thomas', 'Kint', 4, 't.kint@hotmail.fr', 'password', 'password']
+    ]
+
+    user_group_list.each do |name, level|
+      UserGroup.create(name: name, level: level)
+    end
+
+    user_list.each do |first_name, last_name, user_group_id, email, password, password_confirmation|
+      User.create!(first_name: first_name, last_name: last_name, user_group_id: user_group_id, email: email, password: password, password_confirmation: password_confirmation)
+    end
 end
