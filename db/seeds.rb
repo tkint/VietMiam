7fca80d460ec592e3b39e58ba8d66f0dde0aa0a4
tkint <t.kint@hotmail.fr>
2017-03-23 20:21:02 +0100

    Retrait du config.yml du .gitignore Modification du seeds.rb pour correspondre à la base de production

--- db/seeds.rb
+++ db/seeds.rb
@@ -84,22 +84,34 @@ case Rails.env
         ['Thomas', 'Kint', 24, 't.kint@hotmail.fr', 'password', 'password']
     ]
 
+    # user_group_id
+    # base_entity_id
+    # create
+    # read
+    # update_self
+    # update_lesser_group
+    # update_actual_group
+    # update_upper_group
+    # delete_self
+    # delete_lesser_group
+    # delete_actual_group
+    # delete_upper_group
     rights_list = [
-        [4, 4, 0, 1, 0, 0],
-        [4, 14, 0, 1, 0, 0],
-        [4, 24, 0, 1, 0, 0],
-        [4, 34, 0, 1, 0, 0],
-        [4, 44, 0, 1, 0, 0],
-        [14, 4, 0, 1, 0, 0],
-        [14, 14, 0, 1, 0, 0],
-        [14, 24, 0, 1, 0, 0],
-        [14, 34, 0, 1, 0, 0],
-        [14, 44, 0, 1, 0, 0],
-        [24, 4, 1, 1, 1, 1],
-        [24, 14, 1, 1, 1, 1],
-        [24, 24, 1, 1, 1, 1],
-        [24, 34, 1, 1, 1, 1],
-        [24, 44, 1, 1, 1, 1]
+        [4, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 14, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 24, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [14, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [14, 14, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [14, 24, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [14, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [14, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [24, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [24, 14, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [24, 24, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [24, 34, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [24, 44, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
 
     entities_list.each do |singular, plural|
@@ -114,7 +126,7 @@ case Rails.env
       User.create!(first_name: first_name, last_name: last_name, user_group_id: user_group_id, email: email, password: password, password_confirmation: password_confirmation)
     end
 
-    rights_list.each do |user_group_id, base_entity_id, right_create, right_read, right_update, right_delete|
-      Right.create!(user_group_id: user_group_id, base_entity_id: base_entity_id, right_create: right_create, right_read: right_read, right_update: right_update, right_delete: right_delete)
+    rights_list.each do |uid, eid, c, r, us, ul, ua, uu, ds, dl, da, du|
+      Right.create!(user_group_id: uid, base_entity_id: eid, right_create: c, right_read: r, right_update_self: us, right_update_lesser_group: ul, right_update_actual_group: ua, right_update_upper_group: uu, right_delete_self: ds, right_delete_lesser_group: dl, right_delete_actual_group: da, right_delete_upper_group: du)
     end
 end
